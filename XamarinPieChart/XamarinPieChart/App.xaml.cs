463c07db3e0aebf5a1ab4e03504ebc13d0d97543
sudarvizhi <sudarvizhi3094@gmail.com>
2017-01-12 18:17:05 +0530

    adding pie chart in tamarin forms

---
+++ XamarinPieChart/XamarinPieChart/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinPieChart
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			TabbedPage pages = new TabbedPage();
+			pages.Children.Add(new SimplestPage() { Title = "Simplest" });
+			pages.Children.Add(new NormalPage() { Title = "Normal" });
+			pages.Children.Add(new AreaPage() { Title = "Area" });
+
+			MainPage = pages;
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ XamarinPieChart/XamarinPieChart/Pages/Area/AreaCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinPieChart
+{
+	public partial class AreaCell : ViewCell
+	{
+		public AreaCell()
+		{
+			InitializeComponent();
+		}
+	}
+}
